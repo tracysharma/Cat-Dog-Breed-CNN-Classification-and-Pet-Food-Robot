e8a273739e7268cae729caf732254f485133062d
KyleNonisUTS <160322191+KyleNonisUTS@users.noreply.github.com>
2024-05-26 21:48:52 +1000

    Robot arm stuff

---
+++ RobotArm_Final/panda_gym/envs/tasks/slide.py
@@ -0,0 +1,141 @@
+from typing import Any, Dict
+
+import numpy as np
+
+from panda_gym.envs.core import PyBulletRobot, Task
+from panda_gym.utils import distance
+
+from random import randint, choice
+
+
+class Slide(Task):
+    def __init__(
+        self,
+        sim,
+        reward_type="dense",
+        distance_threshold=0.05,
+        goal_xy_range=0.3,
+        goal_x_offset=0.4,
+        obj_xy_range=0.3,
+    ) -> None:
+        super().__init__(sim)
+
+        self.reward_type = reward_type
+        self.distance_threshold = distance_threshold
+        self.object_size = 0.075
+        self.goal_range_low = np.array([-goal_xy_range / 2 + goal_x_offset, -goal_xy_range / 2, 0])
+        self.goal_range_high = np.array([goal_xy_range / 2 + goal_x_offset, goal_xy_range / 2, 0])
+        self.obj_range_low = np.array([-obj_xy_range / 2, -obj_xy_range / 2, 0])
+        self.obj_range_high = np.array([obj_xy_range / 2, obj_xy_range / 2, 0])
+        with self.sim.no_rendering():
+            self._create_scene()
+
+    def _create_scene(self) -> None:
+        self.sim.create_plane(z_offset=-0.4)
+        self.sim.create_table(length=1.9, width=1.4, height=0.4, x_offset=-0.1)
+        self.sim.create_cylinder(
+            body_name="objectCat",
+            mass=0.2,
+            radius=self.object_size / 2,
+            height=self.object_size,
+            position=np.array([-0.2, 0.23, self.object_size / 2]),
+            rgba_color=np.array([0.1, 0.9, 0.1, 1.0]),
+            lateral_friction=0.1,
+        )
+        self.sim.create_cylinder(
+            body_name="objectDog",
+            mass=0.2,
+            radius=self.object_size / 2,
+            height=self.object_size,
+            position=np.array([-0.2, -0.23, self.object_size / 2]),
+            rgba_color=np.array([0.9, 0.1, 0.1, 1.0]),
+            lateral_friction=0.1,
+        )
+        self.sim.create_cylinder(
+            body_name="target",
+            mass=0.0,
+            ghost=True,
+            radius=self.object_size / 2,
+            height=self.object_size / 2,
+            position=np.array([0.1, 0.0, self.object_size / 2]),
+            rgba_color=np.array([0.9, 0.9, 0.1, 0.3]),
+        )
+
+    def get_obs(self) -> np.ndarray:
+        objectCat_position = np.array(self.sim.get_base_position("objectCat"))
+        objectCat_position_xy = objectCat_position[:3]
+        objectDog_position = np.array(self.sim.get_base_position("objectDog"))
+        objectDog_position_xy = objectDog_position[:3]
+
+        observation = np.concatenate(
+            [
+                objectCat_position_xy,
+                objectDog_position_xy,
+                np.array([self.animal]) # Provided to supply which animal has been chosen
+            ]
+        )
+        return observation
+
+    def get_achieved_goal(self) -> np.ndarray:
+        # print("Goal selected: ", self.animal)
+        if self.animal == 0:
+            # print("Cat")
+            good_object_position = np.array(self.sim.get_base_position("objectCat"))
+        else:
+            # print("Dog")
+            good_object_position = np.array(self.sim.get_base_position("objectDog"))
+        return good_object_position.copy()
+
+    def reset(self) -> None:
+        # THIS SELECTS THE GOAL (0 = cat, 1 = dog)
+        self.animal = randint(0, 1) # For training, choose randomly
+        # self.animal = 1.0 # DOG
+        # self.animal = 0.0 # CAT
+
+        self.goal = self._sample_goal()
+        # object_position = self._sample_object()
+        self.sim.set_base_pose("target", self.goal, np.array([0.2, 0.0, 0.0, 1.0]))
+        self.sim.set_base_pose("objectCat", self._sample_object(0), np.array([0.0, 0.1, 0.0, 1.0]))
+        self.sim.set_base_pose("objectDog", self._sample_object(1), np.array([0.0, -0.1, 0.0, 1.0]))
+
+    def _sample_goal(self) -> np.ndarray:
+        """Randomize goal."""
+        goal = np.array([0.0, 0.0, 0.0])  # z offset for the cube center
+        # noise = self.np_random.uniform(self.goal_range_low, self.goal_range_high)
+        # goal += noise
+        return goal.copy()
+
+    def _sample_object(self, animal) -> np.ndarray:
+        """Randomize start position of object."""
+        if animal == 0:
+            object_position = np.array([-0.2, 0.23, self.object_size / 2])
+        else:
+            object_position = np.array([-0.2, -0.23, self.object_size / 2])
+        # noise = self.np_random.uniform(self.obj_range_low, self.obj_range_high)
+        # object_position += noise
+        return object_position
+
+    def is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> np.ndarray:
+        d = distance(achieved_goal, desired_goal)
+        return np.array(d < self.distance_threshold, dtype=bool)
+
+    def compute_reward(self, observation, achieved_goal, desired_goal, info: Dict[str, Any]) -> np.ndarray:
+
+        observationCopy = observation.copy()
+
+        achieved_goal_copy = achieved_goal.copy()
+
+        # First training reward (Moving behind the can in preparation to push forward)
+        # (1500 episodes)
+        achieved_goal_copy[0] -= 0.1 # offset to behind the can relative to the goal
+        achieved_goal_copy[2] += 0.025 # z-offset to avoid the end effector touching the ground
+        d = distance(observationCopy[:3], achieved_goal_copy[:3])
+
+        # Second training reward (Moving the can forward towards the goal)
+        # (1500 episodes)
+        # achieved_goal_copy[0] -= 0.08 # Almost touching the object
+        # achieved_goal_copy[2] += 0.025
+        # d = distance(observationCopy[:3], achieved_goal_copy[:3])
+        # d = d + 3*distance(achieved_goal, desired_goal) # Heavily reward moving correct can to goal
+
+        return -d.astype(np.float32)
